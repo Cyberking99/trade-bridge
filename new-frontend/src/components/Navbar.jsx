bd0380e729c2c536aac279d4671d8f94b68e61da
Emmanuel Doji <devopsnormad@gmail.com>
2024-09-30 15:24:30 +0100

    Replaced old content with new files

--- new-frontend/src/components/Navbar.jsx
+++ new-frontend/src/components/Navbar.jsx
@@ -46,48 +46,50 @@ const Navbar = () => {
     <div className='bg-gray-900 pt-5'>
       {/* Top Navigation Bar */}
       <header className="flex justify-between items-center py-4 px-10 border rounded-full mx-5 bg-gray-800">
+       <Link to="/hero">
         <div className="text-2xl font-bold flex gap-2 items-center">
           <img
             src={Logo}
             alt="Trade Bridge Logo"
             className="w-10 md:w-[32px]"
           />
-          <span className="mr-4 text-white">Trade<span className='text-orange-500'>Bridge</span></span>
+          <span className="mr-4 text-white">Trade<span className='text-primary-500'>Bridge</span></span>
         </div>
+        </Link>
 
         <div className="flex items-center space-x-4">
           {/* Wallet Connect Button */}
           {isConnected && typeof currentAccount === 'string' ? (
-            <button className="bg-orange-500 text-white rounded-full px-4 py-2">
+            <button className="bg-primary-500 text-white rounded-full px-4 py-2">
               {`${currentAccount.substring(0, 6)}...${currentAccount.substring(currentAccount.length - 4)}`}
             </button>
           ) : (
             <button 
               onClick={connectWallet} 
-              className="bg-orange-500 text-white rounded-full px-4 py-2"
+              className="bg-primary-500 text-white rounded-full px-4 py-2"
             >
               Connect Wallet
             </button>
           )}
 
-          <Link to="/seller-dashboard/create-commodity" className="px-4 text-white py-2 bg-gray-700 border border-orange-500 rounded-full flex items-center">
-            Add Product <FaPlus className="ml-2 bg-orange-500 w-6 h-6 p-2 rounded-full" /> 
+          <Link to="/seller-dashboard/create-commodity" className="px-4 text-white py-2 bg-gray-700 border border-primary-500 rounded-full flex items-center">
+            Add Product <FaPlus className="ml-2 bg-primary-500 w-6 h-6 p-2 rounded-full" /> 
           </Link>
         </div>
       </header>
 
       <div className="flex justify-between space-x-4 py-4 px-10 border rounded-full mx-5 mt-4 bg-gray-800">
         <div className='flex gap-4'>
-          <Link to="seller-dashboard/my-commodity" className="px-4 py-2 bg-gray-700 border text-white border-orange-500 hover:bg-orange-500 rounded-full flex items-center">
+          <Link to="seller-dashboard/my-commodity" className="px-4 py-2 bg-gray-700 border text-white border-primary-500 hover:bg-primary-500 rounded-full flex items-center">
             My Products <FaBoxOpen className="ml-2" /> 
           </Link>
-          <Link to="/disputes" className="px-4 py-2 bg-gray-700 border text-white border-orange-500 hover:bg-orange-500 rounded-full flex items-center">
-            Disputes <FaGavel className="ml-2" /> 
+          <Link to="/disputes" className="px-4 py-2 bg-gray-700 border text-white border-primary-500 hover:bg-primary-500 rounded-full flex items-center">
+            Disputes <FaGavel className="ml-2 text-white" /> 
           </Link>
         </div>
         <div>
-          <Link to="/create-ticket" className="px-4 py-2 bg-gray-700 border border-orange-500 text-white rounded-full flex items-center">
-            Create a ticket <FaTicketAlt className="ml-2 bg-orange-500 w-7 h-7 p-2 rounded-full" /> 
+          <Link to="/create-ticket" className="px-4 py-2 bg-gray-700 border border-primary-500 text-white rounded-full flex items-center">
+            Create a ticket <FaTicketAlt className="ml-2 bg-primary-500 w-7 h-7 p-2 rounded-full" /> 
           </Link>
         </div>
       </div>
